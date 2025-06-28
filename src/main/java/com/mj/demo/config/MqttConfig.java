c401823ea66663dd7401d371bc7ca8fd52fd851d
RK-LJ <junjie_lu@raykol.com>
2025-06-28 10:46:45 +0800

    fix:增加注释

--- src/main/java/com/mj/demo/config/MqttConfig.java
+++ src/main/java/com/mj/demo/config/MqttConfig.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * mqtt配置类
+ */
 @Getter
 @Configuration
 public class MqttConfig {
